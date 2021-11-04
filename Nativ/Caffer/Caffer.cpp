9947cfb053a5b0e441f853dc2b0625878ca2cd7a
Mark Toth <tothmarkandor99@freemail.hu>
2021-11-04 02:19:17 +0100

    Ciff to bmp conversion completed

--- Nativ/Caffer/Caffer.cpp
+++ Nativ/Caffer/Caffer.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include <string.h>
+#include <ios>
 
 using std::vector;
 using std::cout;
@@ -42,22 +43,22 @@ vector<unsigned char> readFile(const char* filename)
 	return vec;
 }
 
-uint64_t read8BytesIntBe(const vector<unsigned char>& in,const uint64_t start) {
-	if (in.size() < start + 8) {
+uint64_t read8ByteIntBe(const vector<unsigned char>& ciff, const uint64_t start) {
+	if (ciff.size() < start + 8) { // TODO: overflow detection
 		throw new exception(); // Index out of bounds
 	}
 	uint64_t accumulator = 0;
-	for (uint64_t i = start; i < start + 8; i++) {
-		accumulator = (accumulator << 8) | (uint64_t)in[i];
+	for (uint64_t i = start; i < start + 8; i++) { // TODO: overflow detection
+		accumulator = (accumulator << 8) | (uint64_t)ciff[i];
 	}
 	return accumulator;
 }
 
-string readCaption(const vector<unsigned char>& in, const uint64_t start, const uint64_t headerSize) {
+string readCaption(const vector<unsigned char>& ciff, const uint64_t start, const uint64_t headerSize) {
 	vector<unsigned char> chars;
 	uint64_t i = start;
-	while (i < headerSize && in[i] != '\n') {
-		chars.push_back(in[i++]);
+	while (i < headerSize && ciff[i] != '\n') {
+		chars.push_back(ciff[i++]); // TODO: overflow detection
 	}
 	if (i == headerSize) {
 		throw new exception(); // Caption missing \n terminator
@@ -66,54 +67,77 @@ string readCaption(const vector<unsigned char>& in, const uint64_t start, const 
 	return text;
 }
 
-vector<string> readTags(const vector<unsigned char>& in, const uint64_t start, const uint64_t headerSize) {
+vector<string> readTags(const vector<unsigned char>& ciff, const uint64_t start, const uint64_t headerSize) {
 	vector<string> tags;
 	uint64_t i = start;
-	if (in[headerSize - 1] != '\0') {
+	if (ciff[headerSize - 1] != '\0') { // TODO: undeflow detection
 		throw new exception(); // Tags not properly terminated
 	}
 	vector<unsigned char> currentTag;
 	while (i < headerSize) {
-		if (in[i] == '\0') {
+		if (ciff[i] == '\0') {
 			string text(currentTag.begin(), currentTag.end());
 			tags.push_back(text);
 			currentTag.clear();
 		}
 		else {
-			currentTag.push_back(in[i]);
+			currentTag.push_back(ciff[i]);
 		}
-		i++;
+		i++; // TODO: overflow detection
 	}
 	return tags;
 }
 
+void bmpAddNumberLe(vector<unsigned char>& bmp, const uint64_t number, const uint64_t bytes) {
+	for (size_t i = 0; i < bytes; i++) { // TODO: overflow detection
+		bmp.push_back((unsigned char)(number >> (i * 8)));
+	}
+}
+
 void bmpAddHeader(vector<unsigned char>& bmp, const uint64_t bmpSize) {
-	bmp.push_back(0x42);
-	bmp.push_back(0x4d);
-	bmp.push_back((unsigned char)(bmpSize >> 0));
-	bmp.push_back((unsigned char)(bmpSize >> 1));
-	bmp.push_back((unsigned char)(bmpSize >> 2));
-	bmp.push_back((unsigned char)(bmpSize >> 3));
-	bmp.push_back(0x00);
-	bmp.push_back(0x00);
-	bmp.push_back(0x00);
-	bmp.push_back(0x00);
-	bmp.push_back(0x36);
-	bmp.push_back(0x00);
-	bmp.push_back(0x00);
-	bmp.push_back(0x00);
+	bmp.push_back(0x42); // B
+	bmp.push_back(0x4d); // M
+	bmpAddNumberLe(bmp, bmpSize, 4); // File size
+	bmpAddNumberLe(bmp, 0, 2);
+	bmpAddNumberLe(bmp, 0, 2);
+	bmpAddNumberLe(bmp, 54, 4); // Pixel array offset
 }
 
 void bmpAddDibHeader(vector<unsigned char>& bmp, const uint64_t width, const uint64_t height) {
-	bmp.push_back(0x28);
-	bmp.push_back(0x00);
-	bmp.push_back(0x00);
-	bmp.push_back(0x00);
+	bmpAddNumberLe(bmp, 40, 4); // DIB header length
+	bmpAddNumberLe(bmp, width, 4); // Width
+	bmpAddNumberLe(bmp, height, 4); // Height
+	bmpAddNumberLe(bmp, 1, 2); // Number of planes
+	bmpAddNumberLe(bmp, 24, 2); // Bits per pixel
+	bmpAddNumberLe(bmp, 0, 4); // Compression mode
+	bmpAddNumberLe(bmp, width * height * 4, 4); // Byte size of bitmap data with paddings // TODO: overflow detection
+	bmpAddNumberLe(bmp, 2835, 4); // Horizontal print resolution
+	bmpAddNumberLe(bmp, 2835, 4); // Verticel print resolution
+	bmpAddNumberLe(bmp, 0, 4); // Number of colors in color palette
+	bmpAddNumberLe(bmp, 0, 4); // Important colors
 }
 
-int main()
-{
-	vector<unsigned char> ciff = readFile("c:\\Users\\Mark\\Desktop\\MSc\\sz�m�t�g�pes biztons�g\\IT_security\\Nativ\\Caffer\\Debug\\test1.ciff");
+void bmpAddPixelArray(vector<unsigned char>& bmp, const vector<unsigned char>& ciff, const uint64_t start, const uint64_t width, const uint64_t height) {
+	for (uint64_t i = 0; i < height; i++) {
+		for (uint64_t j = 0; j < width; j++) {
+			uint64_t offset = start + (i * width + j) * 3 ; // TODO: overflow detection
+			bmp.push_back(ciff[offset + 2]); // TODO: overflow detection
+			bmp.push_back(ciff[offset + 1]); // TODO: overflow detection
+			bmp.push_back(ciff[offset + 0]); // TODO: overflow detection
+		}
+		uint64_t padding = 4 - (width * 3) % 4; // TODO: overflow detection
+		for (uint64_t j = 0; j < padding; j++) { // TODO: overflow detection
+			bmp.push_back(0x00);
+		}
+	}
+}
+
+void writeToFile(vector<unsigned char>& data, string filename) {
+	std::ofstream outfile(filename, std::ios::out | std::ios::binary);
+	outfile.write((const char *)&data[0], data.size());
+}
+
+vector<unsigned char> ciffToBmp(vector<unsigned char>& ciff) {
 	const size_t minFileSize = 38; // magic (4) + header_size (8) + content_size(8) + width (8) + height (8) + caption (>1) + tags (>1)
 	const size_t fileSize = ciff.size();
 	if (fileSize < minFileSize) {
@@ -122,13 +146,13 @@ int main()
 	if (!(ciff[0] == 'C' && ciff[1] == 'I' && ciff[2] == 'F' && ciff[3] == 'F')) {
 		throw new exception(); // Invalid magic
 	}
-	uint64_t headerSize = read8BytesIntBe(ciff, 4);
+	uint64_t headerSize = read8ByteIntBe(ciff, 4);
 	if (headerSize > fileSize) {
 		throw new exception(); // Header size bigger than file size
 	}
-	uint64_t contentSize = read8BytesIntBe(ciff, 12);
-	uint64_t width = read8BytesIntBe(ciff, 20);
-	uint64_t height = read8BytesIntBe(ciff, 28);
+	uint64_t contentSize = read8ByteIntBe(ciff, 12);
+	uint64_t width = read8ByteIntBe(ciff, 20);
+	uint64_t height = read8ByteIntBe(ciff, 28);
 	if (fileSize != headerSize + contentSize) { // TODO: overflow detection
 		throw new exception(); // Total size mismatch
 	}
@@ -136,16 +160,22 @@ int main()
 		throw new exception(); // Content size mismatch
 	}
 	string caption = readCaption(ciff, 36, headerSize);
-	vector<string> tags = readTags(ciff, 36 + (uint64_t)caption.length() + 1, headerSize);
+	vector<string> tags = readTags(ciff, 36 + (uint64_t)caption.length() + 1, headerSize); // TODO: overflow detection
 
-	printf("%d\n", fileSize);
-	printf("%ld\n", (unsigned long)headerSize);
-	printf("%ld\n", (unsigned long)contentSize);
-	printf("%ld\n", (unsigned long)width);
-	printf("%ld\n", (unsigned long)height);
-	printf("%s\n", caption.c_str());
-	printf("%d\n", tags.size());
-	for (int i = 0; i < tags.size(); i++) {
-		printf("%s\n", tags[i].c_str());
-	}
+	vector<unsigned char> bmp;
+	uint64_t bmpSize = 54 + ((width * 3 + 3) / 4) * 4 * height; // TODO: overflow detection
+	bmpAddHeader(bmp, bmpSize);
+	bmpAddDibHeader(bmp, width, height);
+	bmpAddPixelArray(bmp, ciff, headerSize, width, height);
+
+	return bmp;
+}
+
+int main()
+{
+	vector<unsigned char> ciff = readFile("c:\\Users\\Mark\\Desktop\\MSc\\számítógépes biztonság\\IT_security\\Nativ\\Caffer\\Debug\\test1.ciff");
+
+	vector<unsigned char> bmp = ciffToBmp(ciff);
+
+	writeToFile(bmp, "c:\\Users\\Mark\\Desktop\\MSc\\számítógépes biztonság\\IT_security\\Nativ\\Caffer\\Debug\\test1.bmp");
 }
